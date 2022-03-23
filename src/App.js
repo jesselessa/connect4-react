6e8facaed6d2614b21d9dc719ee338ddf1bb81ec
jess <jessica.elessa@gmail.com>
2022-03-23 17:30:13 +0100

    created Rules component and replaced it in render instead of <aside>

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ import React from "react";
 import "./App.css";
 // COMPONENT
 import Row from "./components/Row";
-// IMAGE
-import children from "./assets/children.png";
+import Rules from "./components/Rules";
 
 class App extends React.Component {
   // State
@@ -197,39 +196,7 @@ class App extends React.Component {
     return (
       <main>
         <section>
-          <aside className="gameRules">
-            <div className="logoTitle">
-              <p className="title">
-                <span className="letterM">M</span>
-                <span className="letterB">B</span>
-              </p>
-              <p className="subtitle">JEUX</p>
-            </div>
-
-            <div className="ref">
-              <img className="children" src={children} alt="children" />
-              <p>6 ans-Adulte</p>
-              <p className="players">2 joueurs</p>
-              <div className="separator"></div>
-            </div>
-
-            <div className="rules">
-              <p>
-                Insérez un jeton dans la grille à tour de rôle. Le premier
-                joueur qui réussit à aligner quatre jetons (horizontalement,
-                verticalement ou diagonalement) gagne la partie.
-              </p>
-              <p>
-                Si toutes les cases de la grille sont remplies et qu'aucun des
-                deux joueurs n'a réalisé un tel alignement, la partie est
-                déclarée nulle.
-              </p>
-              <p>
-                Appuyez sur le bouton "Reset" pour commencer une nouvelle
-                partie.
-              </p>
-            </div>
-          </aside>
+          <Rules />
 
           <div className="titleAndBoard">
             <div className="boardTitle">
