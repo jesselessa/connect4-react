3e3f66e87333917c2cf1032e33b5edcf604993f2
jesselessa <jessica.elessa@gmail.com>
2025-03-18 03:58:46 +0100

    updated vite configuration file and package-lock.json

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/connect4-react/",
-  server: {
-    port: 5173,
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: "[name].[hash].js",
-      },
-    },
-  },
 });
